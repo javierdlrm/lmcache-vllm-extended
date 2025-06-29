7e5fda4f488b5a0913490c295bdd641f133301c1
Javier de la Rúa Martínez <javier@logicalclocks.com>
2025-06-29 18:10:29 +0200

    Fixes

--- frontend/run_experiment.py
+++ frontend/run_experiment.py
@@ -89,11 +89,9 @@ def main():
     # Run requests
     generator = RequestGenerator(session_context_prompts_dict)
     for prompt, response_chunk in generator.start():
-        print(
-            f"Prompt: {prompt}\nResponse chunk: {response_chunk}\n{'-'*40}",
-            end="\r",
-            flush=True,
-        )
+        output = f"Prompt: {prompt}\nResponse chunk: {response_chunk}\n{'-'*40}"
+        print("\r" + " " * 80, end="\r")
+        print(output, end="\r", flush=True)
 
 
 if __name__ == "__main__":
