c5879f15529fe9a8bf826564055869f8464d079e
Javier de la Rúa Martínez <javier@logicalclocks.com>
2025-07-04 16:51:18 +0200

    Reshape before search

--- lmcache_vllm/rag.py
+++ lmcache_vllm/rag.py
@@ -39,7 +39,9 @@ class RAG:
         )
         question_np = question_embedding.cpu().numpy().reshape(1, -1).astype(np.float32)
         distances, ids = self.faiss_index.search(question_np, top_k)
-        context_key = self.context_keys[ids[0][0]]
+        print("//// Distances: ", distances)
+        print("//// IDs: ", ids)
+        context_key = self.context_keys[ids[0]]
         context = self.contexts[context_key]
         return context_key, context
 
